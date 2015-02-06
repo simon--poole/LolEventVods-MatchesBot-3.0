1591f2da75d63b81ff81a104be8e0df8829fd1e6
simon--poole <simon__poole@hotmail.com>
2015-02-06 22:35:17 +0000

    Improved config documentation.

--- config.php
+++ config.php
@@ -3,19 +3,21 @@ class Config {
 	static $Settings = array(
 		"subreddit"=>"LEVbeta",				//Subreddit for bot to run on
 		"spoilers"=>array("final","ro8","ro16","playoff","iem","tiebreaker"),
-		"no_games"=>"No upcoming matches",		//Message when no upcoming matc
-		"error_msg"=>"Beep Boop. Something broke!",	//Something broke
-		"error_user"=>"SatansF4TE",			//User to send error messages to.
 	);
+	//Wikipage that bot will access to load sidebar details
+	//Make sure your bot account has access to it
 	static $wiki = array(
 		"page"=>"sidebarbot",
 		"template"=>"%%matches%%",
 	);
+	//User account details
+	//These aren't real, don't bother trying them
 	static $User = array(
 		"user"=>'LEVMatchesBot',
 		"passwd"=>"rl0bUf{9EtTCpnU",
 		"api_type"=>"json",
 	);
+	//For some odd reason Riot's API sometimes leaves the shortName field empty, so when it does we refer to this array
 	static $Teams = array(
 		"Energy PACEMAKER"=>"EP",
 		"Team WE"=>"WE",
